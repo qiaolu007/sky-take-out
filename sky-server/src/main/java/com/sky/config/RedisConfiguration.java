8a773740be2e2fcb8530913c9e849f541a992863
qiaolu007 <lijunchn9@163.com>
2025-04-17 16:35:15 +0800

    ［需求／BUG］: day5 Spring Data Redis 操作示例 ［模块］: ［说明］: ［作者］: 桥泸007

---
+++ sky-server/src/main/java/com/sky/config/RedisConfiguration.java
@@ -0,0 +1,38 @@
+package com.sky.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+@Slf4j
+public class RedisConfiguration {
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        log.info("开始创建redis模板对象...");
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+
+        // 设置redis的连接工厂对象
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+
+        // 使用StringRedisSerializer来序列化和反序列化redis的key
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+
+        // 使用GenericJackson2JsonRedisSerializer来序列化和反序列化redis的value
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+
+        // 设置hash key & value序列化模式
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+
+        // 初始化参数设置
+//        redisTemplate.afterPropertiesSet();
+
+        return redisTemplate;
+    }
+}

65de6547617a20bba701ecdcb9c82e8d647e2d3d
qiaolu007 <lijunchn9@163.com>
2025-04-17 17:20:30 +0800

    ［需求／BUG］: day5 - spring data Redis使用/营业状态设置 ［模块］: ［说明］: ［作者］: 桥泸007

--- sky-server/src/main/java/com/sky/config/RedisConfiguration.java
+++ sky-server/src/main/java/com/sky/config/RedisConfiguration.java
@@ -13,9 +13,9 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 public class RedisConfiguration {
 
     @Bean
-    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+    public RedisTemplate redisTemplate(RedisConnectionFactory redisConnectionFactory) {
         log.info("开始创建redis模板对象...");
-        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        RedisTemplate redisTemplate = new RedisTemplate<>();
 
         // 设置redis的连接工厂对象
         redisTemplate.setConnectionFactory(redisConnectionFactory);
---
+++ sky-server/src/main/java/com/sky/controller/admin/user/ShopController.java
@@ -0,0 +1,32 @@
+package com.sky.controller.admin.user;
+
+import com.sky.result.Result;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.*;
+
+@RestController("userShopController")
+@RequestMapping("/user/shop")
+@Slf4j
+@Api(tags = "店铺相关接口")
+public class ShopController {
+
+    public static final String KEY = "SHOP_STATUS";
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    /**
+     * 查看店铺营业状态
+     * @return
+     */
+    @GetMapping("/status")
+    @ApiOperation("查看店铺营业状态")
+    public Result getStatus(){
+        Integer status = (Integer) redisTemplate.opsForValue().get(KEY);
+        log.info("查看店铺营业状态：{}", status == 1 ? "营业中" : "打烊中");
+        return Result.success(status);
+    }
+}
